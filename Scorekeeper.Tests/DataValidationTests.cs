1cb091d21415ffcca5bb0ec46a1850405ceb52a8
Yirjham <yirjham@gmail.com>
2021-10-12 17:45:32 +0100

    Unit test for DataValidation class. Red and green for all 3 methods.

---
+++ Scorekeeper.Tests/DataValidationTests.cs
@@ -0,0 +1,63 @@
+﻿using ScorekeeperLibrary.DataValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Scorekeeper.Tests
+{
+    public class DataValidationTests
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("45", false)]
+        [InlineData("hello", false)]
+        public void ShouldReturnTrueWhenEmptyString(string input, bool expected)
+        {
+            // Arrange
+           
+            // Act
+            bool actual = DataValidation.isEmpty(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
+            
+        }
+        [Theory]
+        [InlineData("23", true)]
+        [InlineData("0", true)]
+        [InlineData("4", true)]
+        [InlineData("hello", false)]
+        [InlineData("2.1", false)]
+        public void ShouldReturnTrueForNumericData(string input, bool expected)
+        {
+            // Arrange
+
+            // Act
+            bool actual = DataValidation.isValidNumericData(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("23", true)]
+        [InlineData("0", true)]
+        [InlineData("4", true)]
+        [InlineData("130", false)]
+        [InlineData("-1", false)]
+        [InlineData("0.3", false)]
+        public void ShouldReturnTrueWhenInValidRange(string input, bool expected)
+        {
+            // Arrange
+
+            // Act
+            bool actual = DataValidation.isValidRange(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ ScorekeeperLibrary/DataValidation/DataValidation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorekeeperLibrary.DataValidation
+{
+    public static class DataValidation
+    {
+        // For empty data
+        // For invalid numeric format
+        // For range checking
+        public static bool isEmpty(string userInput)
+        {
+            if (userInput == "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool isValidNumericData(string userInputText)
+        {
+            int userInputInt = 0;
+
+            bool isValid = int.TryParse(userInputText, out userInputInt);
+            if (isValid)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool isValidRange(string userInputText)
+        {
+            int userInputNumber = 0;
+
+            if (isValidNumericData(userInputText) == true)
+            {
+                userInputNumber = int.Parse(userInputText);
+
+                if (userInputNumber >= 0 && userInputNumber < 130)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
