f94d9b04fd6e7b45dab950211eb72ddd6da922c2
Yirjham <yirjham@gmail.com>
2021-10-12 15:42:06 +0100

    User interface messages class with messages for round highest scorer and game winner created. They have been temporarily copied to the class library to unit test

---
+++ ScorekeeperLibrary/UIMessages.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorekeeperLibrary.Models
+{
+    public static class UIMessages
+    {
+        // Round highest scorer messages for 2, 3 and 4 players
+        public static string RoundHighestScorerMessage(PlayerModel player1, PlayerModel player2)
+        {
+            PlayerModel highestScorer = Calculations.DeterminesRoundHighestScorer(player1, player2);
+            return $"{highestScorer.PlayerName} is the winner of this round with {highestScorer.RoundScore} points. Well done!";
+        }
+        public static string RoundHighestScorerMessage(PlayerModel player1, PlayerModel player2, PlayerModel player3)
+        {
+            PlayerModel highestScorer = Calculations.DeterminesRoundHighestScorer(player1, player2, player3);
+            return $"{highestScorer.PlayerName} is the winner of this round with {highestScorer.RoundScore} points. Well done!";
+        }
+        public static string RoundHighestScorerMessage(PlayerModel player1, PlayerModel player2, PlayerModel player3, PlayerModel player4)
+        {
+            PlayerModel highestScorer = Calculations.DeterminesRoundHighestScorer(player1, player2, player3, player4);
+            return $"{highestScorer.PlayerName} is the winner of this round with {highestScorer.RoundScore} points. Well done!";
+        }
+        // Game winner messages for 2, 3 and 4 players
+      
+        public static string GameWinnerMessage(PlayerModel player1, PlayerModel player2)
+        {
+
+        }
+        public static string GameWinnerMessage(PlayerModel player1, PlayerModel player2, PlayerModel player3)
+        {
+
+        }
+        public static string GameWinnerMessage(PlayerModel player1, PlayerModel player2, PlayerModel player3, PlayerModel player4)
+        {
+
+        }
+    }
+
+    
+}
---
+++ WinFormsUI/UIMessages.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorekeeperLibrary.Models
+{
+    public static class UIMessages
+    {
+        // Round highest scorer messages for 2, 3 and 4 players
+        public static string RoundHighestScorerMessage(PlayerModel player1, PlayerModel player2)
+        {
+            PlayerModel highestScorer = Calculations.DeterminesRoundHighestScorer(player1, player2);
+            return $"{highestScorer.PlayerName} is the winner of this round with {highestScorer.RoundScore} points. Well done!";
+        }
+        public static string RoundHighestScorerMessage(PlayerModel player1, PlayerModel player2, PlayerModel player3)
+        {
+            PlayerModel highestScorer = Calculations.DeterminesRoundHighestScorer(player1, player2, player3);
+            return $"{highestScorer.PlayerName} is the winner of this round with {highestScorer.RoundScore} points. Well done!";
+        }
+        public static string RoundHighestScorerMessage(PlayerModel player1, PlayerModel player2, PlayerModel player3, PlayerModel player4)
+        {
+            PlayerModel highestScorer = Calculations.DeterminesRoundHighestScorer(player1, player2, player3, player4);
+            return $"{highestScorer.PlayerName} is the winner of this round with {highestScorer.RoundScore} points. Well done!";
+        }
+        // Game winner messages for 2, 3 and 4 players
+      
+        public static string GameWinnerMessage(PlayerModel player1, PlayerModel player2)
+        {
+            PlayerModel winner = Calculations.DeterminesWinner(player1, player2);
+            return $"{ winner.PlayerName } is today's winner. Congratulations!!!";
+        }
+        public static string GameWinnerMessage(PlayerModel player1, PlayerModel player2, PlayerModel player3)
+        {
+            PlayerModel winner = Calculations.DeterminesWinner(player1, player2, player3);
+            return $"{ winner.PlayerName } is today's winner. Congratulations!!!";
+        }
+        public static string GameWinnerMessage(PlayerModel player1, PlayerModel player2, PlayerModel player3, PlayerModel player4)
+        {
+            PlayerModel winner = Calculations.DeterminesWinner(player1, player2, player3, player4);
+            return $"{ winner.PlayerName } is today's winner. Congratulations!!!";
+        }
+    }
+
+    
+}
