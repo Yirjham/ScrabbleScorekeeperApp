9fc793ec77bf10b997aec34ae42a00e1757e7f15
Yirjham <yirjham@gmail.com>
2021-10-12 10:35:49 +0100

    Unit Testing - TDD - Red: adding first failing test

---
+++ Scorekeeper.Tests/CalculationTests.cs
@@ -0,0 +1,29 @@
+﻿using ScorekeeperLibrary;
+using ScorekeeperLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Scorekeeper.Tests
+{
+    public class CalculationTests
+    {
+        [Fact]
+        public void ShouldAddUpTwoScores()
+        {
+            // Arrange
+            PlayerModel player = new PlayerModel("testUser") { ScoreSubtotal = 100 };
+            player.RoundScore = 24;
+            int expected = 124;
+
+            // Act
+            Calculations.CalculateCurrentScore(player, player.RoundScore);
+            int actual = player.ScoreSubtotal;
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
