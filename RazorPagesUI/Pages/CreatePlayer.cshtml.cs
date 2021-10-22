2e38e816dc8ad8b0a35795d758f213dc93f37cdf
Yirjham <yirjham@gmail.com>
2021-10-22 08:50:17 +0100

    Adding Razor Pages UI

---
+++ RazorPagesUI/Pages/CreatePlayer.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesUI.Pages
+{
+    public class CreatePlayerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
