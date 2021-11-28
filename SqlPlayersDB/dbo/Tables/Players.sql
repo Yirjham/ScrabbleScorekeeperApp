fbec189d8c64d3a37e919fe92d974f8ffdd2305d
Yirjham <yirjham@gmail.com>
2021-11-28 20:07:40 +0000

    Added SQL Server project to the solution plus the table and published it

---
+++ SqlPlayersDB/dbo/Tables/Players.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Players]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [GamesPlayed] INT NOT NULL, 
+    [GamesWon] INT NOT NULL, 
+    [HighestScore] INT NOT NULL
+)
