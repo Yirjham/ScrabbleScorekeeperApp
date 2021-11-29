45fe111ba0738e88b777e0bb00333a2d4178752e
Yirjham <yirjham@gmail.com>
2021-11-29 11:26:30 +0000

    Added IdLookUp model for getting the id from the database using dapper. Also finished creating the CRUD methods in the Crud class.

--- DataAccessLibrary/Crud.cs
+++ DataAccessLibrary/Crud.cs
@@ -21,7 +21,7 @@ namespace DataAccessLibrary
         {
             string sqlStatement = "select Id, Name, GamesPlayed, GamesWon, HighestScore from dbo.Players";
 
-            return _dataAccess.LoadData<PlayerMapperModel, dynamic>(sql, new { }, _connectionString);
+            return _dataAccess.LoadData<PlayerMapperModel, dynamic>(sqlStatement, new { }, _connectionString);
         }
 
         public void CreatePlayer(PlayerMapperModel player)
@@ -34,13 +34,26 @@ namespace DataAccessLibrary
         public void UpdatePlayerName(string newPlayerName, string currentName)
         {
             // Get ID by using Name
+            string sqlStatement = "select Id from dbo.Players where Name = @Name";
+            int playerID = _dataAccess.LoadData<IdLookUpModel, dynamic>(sqlStatement, new {Name = currentName}, _connectionString).First().Id;
 
             // Update Name by using ID
-            string sqlStatement = "update dbo.Players set Name = @NewName where Name = @CurrentName";
+            sqlStatement = "update dbo.Players set Name = @newPlayerName where Id = @Id";
 
-            // Update using Dapper
 
-           
+            // Update using Dapper based method
+            _dataAccess.SaveData(sqlStatement, new { Name = newPlayerName, Id = playerID }, _connectionString);
+        }
+
+        public void DeletePlayer(string playerName)
+        {
+            // Get id with the player's name
+            string sqlStatement = "select Id from dbo.Players where Name = @Name";
+            int playerId = _dataAccess.LoadData<IdLookUpModel, dynamic>(sqlStatement, new { Name = playerName }, _connectionString).First().Id;
+
+            // Use id to delete player's row in db
+            sqlStatement = "delete from dbo.Players where Id = @Id";
+            _dataAccess.SaveData(sqlStatement, new {Id = playerId}, _connectionString);
         }
     }
 }
